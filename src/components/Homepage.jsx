5ee93f6130ca68129501f37a464642581717f150
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-13 23:07:24 +1000

    Homepage with hero image

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import "../styles/Homepage.scss"
+
+export default function Homepage () {
+    return (
+        <main className="main">
+            <div className="hero-content">
+                <h2>Let&apos;s get stylish.</h2>
+                <a href="#">Shop Now</a>
+            </div>
+        </main>
+    )
+}
