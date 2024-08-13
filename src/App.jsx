ec6856bd796f1fb2be509c89cc2085e3f6974c51
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-13 23:07:57 +1000

    initial render with Header and Homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css'
-
+import Header from './components/Header'
+import Homepage from './components/Homepage'
 function App() {
-
+  return (
+    <>
+       <Header/>
+       <Homepage/>
+    </>
+  )
 }
 
 export default App
