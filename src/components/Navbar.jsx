32367863f85e72f66a884dceec9bdfaa4ca5f5c3
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-14 10:55:00 +1000

    Add more element to Navbar, remove unnecessary styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import "../styles/Navbar.scss"
 
 export default function Navbar() {
     return (
-        <div className="navCtn">
             <div className="navlinks">
                 <a href="shop">Shop</a>
+                <a href="shop">Electronics</a>
+                <a href="shop">Clothes</a>
+                <a href="shop">Hardware</a>
                 <a href="about">About</a>
             </div>
-            <a href="cart"><i className="fa-solid fa-cart-shopping"></i></a>
-        </div>
     )
 }
