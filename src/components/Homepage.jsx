d2036431774e2f31a046137f9ffb939ae5b58e9d
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-14 10:56:18 +1000

    Move <Header/> into Homepage to cover Header background with hero image

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,12 +1,13 @@
 import "../styles/Homepage.scss"
-
+import Header from "./Header"
 export default function Homepage () {
     return (
-        <main className="main">
+        <section className="main">
+            <Header/>
             <div className="hero-content">
                 <h2>Let&apos;s get stylish.</h2>
-                <a href="#">Shop Now</a>
+                <a href="#">SHOP NOW</a>
             </div>
-        </main>
+        </section>
     )
 }
