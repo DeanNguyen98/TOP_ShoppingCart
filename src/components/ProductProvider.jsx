b8cb062f57d3cc74e4db545663a9f15db780cf6a
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-14 23:38:36 +1000

    Product Context Provider to fetch store API

---
+++ src/components/ProductProvider.jsx
@@ -0,0 +1,20 @@
+import { createContext, useEffect, useState } from 'react'
+import PropTypes from 'prop-types';
+
+export const productContext = createContext();
+export const ProductProvider = ({children}) => {
+  const [products, setproducts] = useState([]);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=> setproducts(json));
+  },[]);
+  return (
+    <productContext.Provider value= {products}>
+      {children}
+    </productContext.Provider>
+  )
+}
+ProductProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
