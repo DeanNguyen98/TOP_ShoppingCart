e865c71423a598b6b133d284fc627d019bdef0e1
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-14 23:39:16 +1000

    Add Product Provider logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { ProductProvider } from './components/ProductProvider.jsx';
 import './App.css'
 import Header from './components/Header.jsx'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <>
-      <Header/>
+    <ProductProvider>
+    <Header/>
         <div className="main-section">
           <Outlet/>
         </div>
+    </ProductProvider>
     </>
   )
 }
+
 
 export default App
