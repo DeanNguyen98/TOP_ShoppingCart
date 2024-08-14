bec127a9c8c42584b67e0fc29e264e1cac828005
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-14 21:31:21 +1000

    Add link to homepage for h1 tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./Navbar"
 import "../styles/Header.scss"
+import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
         <div className="header-ctn">
             <div className="header">
-                <h1>NICE.</h1>
+                <h1><Link to="/">NICE</Link>.</h1>
                 <div className="icon-wrapper">
                 <i className="fa-solid fa-magnifying-glass"></i>
                 <a href="cart"><i className="fa-solid fa-cart-shopping"></i></a>
