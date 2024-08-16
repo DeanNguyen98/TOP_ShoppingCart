36afffbf4011fa7a163658fc4ae589ff0f90760a
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-16 23:55:37 +1000

    Change shop path to absolute path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import "../styles/Navbar.scss"
 export default function Navbar() {
     return (
             <div className="navlinks">
-                <a href="shop">Shop</a>
+                <a href="/shop">Shop</a>
                 <a href="#">Electronics</a>
                 <a href="#">Clothes</a>
                 <a href="#">Hardware</a>
