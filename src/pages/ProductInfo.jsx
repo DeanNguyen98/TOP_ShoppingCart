76d03d04a923be4baaeb6d5c73255b7df68aabc3
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-16 23:56:17 +1000

    Finish logic for Product Info page, add styling

--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types"
 import { useContext } from "react"
 import { productContext } from "../components/ProductProvider"
 import { useParams } from "react-router-dom";
+import styles from "./ProductInfo.module.scss";
     export default function ProductInfo () {
     const products = useContext(productContext);
     const { productId } = useParams();
@@ -9,11 +10,24 @@ import { useParams } from "react-router-dom";
     console.log(product);
         return (
             <>
-        <div className="info-ctn">
-            <div className="info-img-wrapper">
-            <img className="info-img" src={product.image}></img>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
+        <div className={styles.nav}>
+            <p>
+                <a href="/">Home</a> &#10148;
+                <a href="/shop"> Shop All </a> &#10148; 
+                 &nbsp;{product.title}
+                    
+            </p>
+
+        </div>
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+            <img className={styles.image} src={product.image}></img>
+            </div>
+            <div className={styles.description}>
+                <h2>{product.title}</h2>
+                <h2>${product.price}</h2>
+                <p>{product.description}</p>
+                <button className={styles.cartButton}>ADD TO CART</button>
             </div>
         </div>
         </>
