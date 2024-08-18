1d613e6a80a6b04a070778dd7cba7253392bc1e1
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-18 21:41:48 +1000

    Add CartProvider to App component, add styling for Link:hover

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ProductProvider } from './components/ProductProvider.jsx';
 import './App.css'
 import Header from './components/Header.jsx'
 import { Outlet } from 'react-router-dom';
+import { CartProvider } from './components/CartProvider.jsx';
 
 
 
@@ -9,10 +10,12 @@ function App() {
   return (
     <>
     <ProductProvider>
-    <Header/>
+      <CartProvider>
+        <Header/>
         <div className="main-section">
           <Outlet/>
         </div>
+      </CartProvider>
     </ProductProvider>
     </>
   )
