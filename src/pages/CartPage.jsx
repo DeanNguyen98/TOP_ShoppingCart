fd0f2252c746b660a105a2d4123c02da41227d89
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-19 20:44:17 +1000

    retrieve data from local storage for cartPage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,21 +1,25 @@
-import { useContext } from "react";
-import { cartContext } from "../components/CartProvider";
+import { useEffect, useState } from "react";
 import "../styles/CartPage.scss";
 export default function CartPage () {
-    const {cart} = useContext(cartContext);
+    const [Storagecart,setStoragecart] = useState([]);
+    useEffect(() => {
+        const savedCart = JSON.parse(localStorage.getItem('cart')) || [];
+        setStoragecart(savedCart);
+    }, []);
     return (
         <div className="cartContainer">
             <div className="cart_header">Check Out</div>
             <div className="cartProductContainer">
-            {cart.length === 0  && (
+            {Storagecart.length === 0  && (
                 <div>Your cart is empty.</div>
             )}
-            {cart.length !== 0 && cart.map(item => {
+            {Storagecart.length !== 0 && Storagecart.map(item => {
                 return (
                     <div className="cart-item" key={item.id}>
                         <img src={item.image}></img>
                         <div className="cart-item-info">
                             <p>{item.title}</p>
+                            <p>Quantity: {item.quantity}</p>
                             <p>${item.price}</p>
                         </div>
                     </div>
