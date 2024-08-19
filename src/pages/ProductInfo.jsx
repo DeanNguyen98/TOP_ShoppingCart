8c7b7c961a1cd43307f70a2365f0ca49ea914d38
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-19 20:44:43 +1000

    add product to cart function

--- src/pages/ProductInfo.jsx
+++ src/pages/ProductInfo.jsx
@@ -1,16 +1,17 @@
 import PropTypes from "prop-types"
 import { useContext, useEffect, useState } from "react"
 import { productContext } from "../components/ProductProvider"
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import styles from "./ProductInfo.module.scss";
-import { Link } from "react-router-dom";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import { cartContext } from "../components/CartProvider";
 
-    export default function ProductInfo () {
-    const [loading, setLoading] = useState(true);
+export default function ProductInfo () {
     const products = useContext(productContext);
+    const [loading, setLoading] = useState(true);
     const { productId } = useParams();
+    const {cart, setCart} = useContext(cartContext);
     const product = products.find(product => product.id === parseInt(productId));
     const recommendedProduct = products.filter(p => p.category === product.category);
     useEffect(() => {
@@ -18,8 +19,9 @@ import 'react-multi-carousel/lib/styles.css';
             setLoading(false);
         }
     }, [products])
-    console.log(recommendedProduct);
-    console.log(product);
+    useEffect(() => {
+        console.log(cart);
+    },[cart]);
     const responsive = {
         superLargeDesktop: {
           // the naming can be any, depends on you.
@@ -39,11 +41,23 @@ import 'react-multi-carousel/lib/styles.css';
           items: 1
         }
       };
+
+      function addProducttoCart () {
+        setCart(prevCart => {
+            const existingProduct = prevCart.find(p => p.id === product.id);
+            if (!existingProduct) {
+                return [...prevCart, {...product, quantity: 1}];
+            } else {
+                return prevCart.map(p => p.id === product.id ? {...p, quantity: p.quantity + 1} : p)
+            }
+            
+        })
+      }
       if (loading) {
         return <div>Loading...</div>
       }
         return (
-            <div className={styles.pagewrapper}>
+        <div className={styles.pagewrapper}>
         <div className={styles.nav}>
             <p>
                 <Link to="/">Home</Link> &#10148;
@@ -53,6 +67,7 @@ import 'react-multi-carousel/lib/styles.css';
             </p>
 
         </div>
+        {/*product section*/}
         <div className={styles.container}>
             <div className={styles.wrapper}>
             <img className={styles.image} src={product.image}></img>
@@ -61,16 +76,29 @@ import 'react-multi-carousel/lib/styles.css';
                 <h2>{product.title}</h2>
                 <h2>${product.price}</h2>
                 <p>{product.description}</p>
-                <button className={styles.cartButton}>ADD TO CART</button>
+                <button 
+                    className={styles.cartButton}
+                    onClick={addProducttoCart}
+                >ADD TO CART</button>
             </div>
         </div>
+
+        {/* Recommended section */}
         <div className={styles.recommendation}>
             <h3>Product you may like</h3>
             <Carousel responsive={responsive} className={styles.carousel} itemClass={styles.carouselItem}>
                 {recommendedProduct.map(rec => {
                     return (
-                        <div key={rec.id} className={styles.recommendedImgWrapper}>
-                            <img src={rec.image}></img>
+                        <div key={rec.id}>
+                            <Link to={`/productInfo/${rec.id}`}>
+                                <div className={styles.recommendedImgWrapper}>
+                                    <img src={rec.image}></img>
+                                </div>
+                            </Link>
+                                <div className={styles.recommendedDescription}>
+                                    <h4>{rec.title}</h4>
+                                    <p>${rec.price}</p>
+                                </div>
                         </div>
                     )
                 })}
