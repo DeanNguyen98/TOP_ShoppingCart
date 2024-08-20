d628dd41b159df8d8033b29a996b1b2b78a6beba
DeanNguyen98 <nctuan.hn@gmail.com>
2024-08-20 23:46:43 +1000

    Cart quantity counter for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
 import Navbar from "./Navbar"
+import CartItemCounter from "./cartItemCounter"
 import "../styles/Header.scss"
 import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { cartContext } from "./CartProvider"
 
 export default function Header() {
+    const {cart} = useContext(cartContext);
     return (
         <div className="header-ctn">
             <div className="header">
-                <h1><Link to="/">NICE</Link>.</h1>
+                <h1 className="header-title"><Link to="/">NICE</Link>.</h1>
                 <div className="icon-wrapper">
                 <i className="fa-solid fa-magnifying-glass"></i>
-                <Link to="cart"><i className="fa-solid fa-cart-shopping"></i></Link>
+                <div className="cart-icon-wrapper">
+                    <Link to="cart"><i className="fa-solid fa-cart-shopping"></i></Link>
+                    <CartItemCounter cart={cart}/>
+                </div>
+                
                 
                 </div>
             </div>
